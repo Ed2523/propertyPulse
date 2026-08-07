9b7c33f9dd6b646546cf9407d650080506efe46d
Edgar <edgar@MacBook-Air-de-Edgar.local>
2026-08-07 14:30:08 -0600

    add more conditional rendering to nav bar items in desktop mode

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -71,12 +71,14 @@ const NavBar = () => {
                 >
                   Properties
                 </Link>
-                <Link
-                  href="/properties/add"
-                  className={`${pathname === "/properties/add" ? "bg-black" : ""} text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
-                >
-                  Add Property
-                </Link>
+                {isLoggedIn && (
+                  <Link
+                    href="/properties/add"
+                    className={`${pathname === "/properties/add" ? "bg-black" : ""} text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
+                  >
+                    Add Property
+                  </Link>
+                )}
               </div>
             </div>
           </div>
