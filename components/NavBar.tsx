c10674fe090c01f8dd9721b627d5fbd2e560397b
Edgar <edgar@MacBook-Air-de-Edgar.local>
2026-08-07 13:56:01 -0600

    add conditiona bg background for nav bar links

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -69,11 +69,11 @@ const NavBar = () => {
                   >Home</Link>
                 <Link
                   href="/properties"
-                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                    className={`${pathname === '/properties' ? 'bg-black' : ''} text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
                   >Properties</Link>
                 <Link
                   href="/properties/add"
-                  className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                   className={`${pathname === '/properties/add' ? 'bg-black' : ''} text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
                   >Add Property</Link>
               </div>
             </div>
